f041ccdbb7600a2c7f42ac0bdd7ba34633fa1741
Aman Nirala <asdevofficial@gmail.com>
2025-07-30 09:41:39 +0530

    Adds Hilt and Deck Details screen
    Enables dependency injection with Hilt.
    
    Implements a Deck Details screen with flashcard swiping
    functionality, delete confirmation, and UI elements.

--- android/app/src/main/java/com/synergyboat/flutterai/MainActivity.kt
+++ android/app/src/main/java/com/synergyboat/flutterai/MainActivity.kt
@@ -13,7 +13,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.synergyboat.flutterai.presentation.router.AppRouter
 import com.synergyboat.flutterai.presentation.ui.theme.FlutterAITheme
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
